0f379926f951a2545573de2ab96fdc43b31281ea
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-03-16 17:53:13 +0100

    Created Empty Unreal Project

---
+++ RacingUnrealProject/Source/RacingUnrealProject/RacingUnrealProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RacingUnrealProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RacingUnrealProject, "RacingUnrealProject" );
