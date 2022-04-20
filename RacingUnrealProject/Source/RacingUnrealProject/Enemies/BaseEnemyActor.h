40806a2a854fabb8ce9eb13c137804b2ac0cae10
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-20 14:19:23 +0200

    Added Index and delegate for handling destruction

--- RacingUnrealProject/Source/RacingUnrealProject/Enemies/BaseEnemyActor.h
+++ RacingUnrealProject/Source/RacingUnrealProject/Enemies/BaseEnemyActor.h
@@ -6,27 +6,30 @@
 #include "GameFramework/Actor.h"
 #include "BaseEnemyActor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnemyDeath, int32, EnemyIndex);
+
 UCLASS()
 class RACINGUNREALPROJECT_API ABaseEnemyActor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ABaseEnemyActor();
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collider")
-		class USphereComponent* SphereComp{nullptr};
+	class USphereComponent* SphereComp{nullptr};
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-		class UStaticMeshComponent* CosmeticMesh{nullptr};
+	class UStaticMeshComponent* CosmeticMesh{nullptr};
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gravity|Spline")
-		class AGravitySplineActor* GravitySplineActive{nullptr};
+	class AGravitySplineActor* GravitySplineActive{nullptr};
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Target|Player Pawn")
-		class ACarPawn* PlayerPawn{nullptr};
-	
+	class ACarPawn* PlayerPawn{nullptr};
+
 	UFUNCTION()
-	virtual void OnBeginOverLap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
-		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnBeginOverLap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                            UPrimitiveComponent* OtherComp,
+	                            int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	FVector GetToPlayerVector(bool bNormalized = true);
 
@@ -39,10 +42,16 @@ protected:
 	bool IsGrounded();
 
 	UPROPERTY(EditDefaultsOnly, Category = "Car")
-		class UArrowComponent* ArrowRayCastStart{ nullptr };
+	class UArrowComponent* ArrowRayCastStart{nullptr};
 
-public:	
+	int32 EnemyArrayIndex;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void SetEnemyArrayIndex(int32 Index) { EnemyArrayIndex = Index; }
+	void HandleDeath();
+
+	FEnemyDeath EnemyDeath;
 };
