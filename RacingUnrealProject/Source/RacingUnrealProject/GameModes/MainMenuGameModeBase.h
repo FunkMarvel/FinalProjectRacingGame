d77080b83325325a0486ddd4d2c785965b9ff035
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-22 15:52:27 +0200

    Created GameModeBase for main menu

---
+++ RacingUnrealProject/Source/RacingUnrealProject/GameModes/MainMenuGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../RacingUnrealProjectGameModeBase.h"
+#include "MainMenuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGUNREALPROJECT_API AMainMenuGameModeBase : public ARacingUnrealProjectGameModeBase
+{
+	GENERATED_BODY()
+	
+};
