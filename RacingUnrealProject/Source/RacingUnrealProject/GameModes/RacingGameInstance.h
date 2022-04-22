8eef4068ef3fdabf94d9dc8fc5c746d33d5115d5
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-22 16:17:58 +0200

    Added GameModeInstance

---
+++ RacingUnrealProject/Source/RacingUnrealProject/GameModes/RacingGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RacingGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGUNREALPROJECT_API URacingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
