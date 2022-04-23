4e4cb2a3eff9ede519fa418a05024de905765eb4
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-23 17:21:01 +0200

    Created Race timer widget

---
+++ RacingUnrealProject/Source/RacingUnrealProject/HUD/TimeAttackWidgets/RaceTimerWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RaceTimerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGUNREALPROJECT_API URaceTimerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
