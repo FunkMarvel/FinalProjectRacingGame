691fc254c89cde412be88519fec807c53c31fcf9
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-24 15:55:06 +0200

    Added timescore save

--- RacingUnrealProject/Source/RacingUnrealProject/GameModes/RacingGameInstance.cpp
+++ RacingUnrealProject/Source/RacingUnrealProject/GameModes/RacingGameInstance.cpp
@@ -5,7 +5,23 @@
 
 #include "Kismet/GameplayStatics.h"
 
+URacingGameInstance::URacingGameInstance()
+{
+	TimeAttackTimes.Reserve(1);
+	ScoreAttackScores.Reserve(1);
+}
+
 void URacingGameInstance::ChangeLevel(FName LevelName)
 {
 	UGameplayStatics::OpenLevel(GetWorld(), LevelName);
+}
+
+void URacingGameInstance::SaveTime(float TimeToSave)
+{
+	TimeAttackTimes.Add(TimeToSave);
+}
+
+void URacingGameInstance::SaveScore(uint32 ScoreToSave)
+{
+	ScoreAttackScores.Add(ScoreToSave);
 }
--- RacingUnrealProject/Source/RacingUnrealProject/GameModes/RacingGameInstance.h
+++ RacingUnrealProject/Source/RacingUnrealProject/GameModes/RacingGameInstance.h
@@ -15,9 +15,24 @@ class RACINGUNREALPROJECT_API URacingGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
+	URacingGameInstance();
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Levels")
 		TArray<FName> LevelNames{};
 
 	UFUNCTION()
 		void ChangeLevel(FName LevelName);
+
+	UFUNCTION()
+		void SaveTime(float TimeToSave);
+
+	UFUNCTION()
+		void SaveScore(uint32 ScoreToSave);
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+		TArray<float> TimeAttackTimes{};
+
+	UPROPERTY(VisibleAnywhere)
+		TArray<uint32> ScoreAttackScores{};
 };
