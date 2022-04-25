ff4eff9aa28480463ec520d662700bf46a06e6d6
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-25 13:34:48 +0200

    Added pause menu

---
+++ RacingUnrealProject/Source/RacingUnrealProject/HUD/PauseMenuWidget.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGUNREALPROJECT_API UPauseMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	class UButton* ResumeButton{nullptr};
+	
+	UPROPERTY(meta=(BindWidget))
+	class UButton* ReturnToMenuButton{nullptr};
+};
