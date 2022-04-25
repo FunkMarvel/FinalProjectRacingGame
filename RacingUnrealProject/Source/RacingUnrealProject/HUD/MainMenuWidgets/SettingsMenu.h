7fb6f06da21814686ad77a940a81e1c57e80cb2c
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-25 14:38:06 +0200

    Added settings menu widget

---
+++ RacingUnrealProject/Source/RacingUnrealProject/HUD/MainMenuWidgets/SettingsMenu.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SettingsMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGUNREALPROJECT_API USettingsMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	class UButton* PlusButton{nullptr};
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* MinusButton{nullptr};
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* SaveButton{nullptr};
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* ReturnToMenuButton{nullptr};
+
+	void UpdateText(int32 NumberToDisplay);
+
+protected:
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* NumLapsTextBlock{nullptr};
+};
