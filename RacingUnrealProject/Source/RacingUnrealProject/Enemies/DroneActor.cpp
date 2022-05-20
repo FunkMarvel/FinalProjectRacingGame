045a6a9fb335a0277bc5aec02a649aa96cef3bbb
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-05-20 03:12:17 +0200

    Update DroneActor.cpp

--- RacingUnrealProject/Source/RacingUnrealProject/Enemies/DroneActor.cpp
+++ RacingUnrealProject/Source/RacingUnrealProject/Enemies/DroneActor.cpp
@@ -7,9 +7,11 @@
 #include "../CarPawn.h"
 #include "../GrappleSphereComponent.h"
 #include "../GravitySplineActor.h"
-#include "../DebugLog.h"
 #include "Components/SphereComponent.h"
 
+/**
+ * @brief Class for drone actor.
+ */
 ADroneActor::ADroneActor()
 {
 	GrappleSphereComponent = CreateDefaultSubobject<UGrappleSphereComponent>(TEXT("GrappleSphereComponent"));
@@ -17,6 +19,7 @@ ADroneActor::ADroneActor()
 	GrappleSphereComponent->SetIsEnabled(true);
 	GrappleSphereComponent->SetIsEatable(true);
 
+	// collider used for AI:
 	SensorSphere = CreateDefaultSubobject<USphereComponent>(TEXT("SensorSphere"));
 	SensorSphere->SetupAttachment(GetRootComponent());
 	SensorSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
@@ -52,13 +55,16 @@ void ADroneActor::Tick(float DeltaSeconds)
 		break;
 	}
 
+	// spawns spiky ball to drop if needed:
 	if (DroppableEnemyClass && bDropEnemy && !bHasSpawned)
 	{
 		FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
 		AttachmentTransformRules.LocationRule = EAttachmentRule::KeepWorld;
 		AttachmentTransformRules.ScaleRule = EAttachmentRule::KeepWorld;
+		
 		FVector SpawnLocation{GetActorLocation() - SpawnOffset*GetActorUpVector()};
 		DroppedEnemyActor = GetWorld()->SpawnActor<ASpikyBallEnemyActor>(DroppableEnemyClass, SpawnLocation, GetActorRotation());
+		
 		if (GravitySplineActive) DroppedEnemyActor->GravitySplineActive = GravitySplineActive;
 		DroppedEnemyActor->AttachToActor(this, AttachmentTransformRules);
 		DroppedEnemyActor->ChangeState(ASpikyBallEnemyActor::Idle);
@@ -70,6 +76,7 @@ void ADroneActor::InterceptingState()
 {
 	if (bEnteringState)
 	{
+		// all drones spend same time to reach target no matter the distance.
 		InterceptSpeed = (TargetLocation-GetActorLocation()).Size()/InterceptTime;
 		bEnteringState = false;
 	}
@@ -89,11 +96,13 @@ void ADroneActor::AttackingState()
 		bEnteringState = false;
 	}
 
+	// look at player
 	FRotator CurrentRot{GetActorRotation()};
 	FRotator TargetRot{(PlayerPawn->GetActorLocation()-GetActorLocation()).Rotation()};
 	FRotator NewRot{FMath::RInterpTo(CurrentRot, TargetRot, GetWorld()->GetDeltaSeconds(), 5.f)};
 	SetActorRotation(NewRot);
-	
+
+	// drops spiky ball:
 	if (DroppedEnemyActor && DropTimer >= DropTime && bDropEnemy && bHasSpawned)
 	{
 		// FDetachmentTransformRules DetachmentTransformRules(EDetachmentRule::KeepWorld, false);
@@ -113,16 +122,17 @@ void ADroneActor::AttackingState()
 
 void ADroneActor::Grappled(FTransform SphereCompTransform)
 {
-	//TODO: Add behaviour for being grappled.
 	if (GameModeBase) GameModeBase->AddScore(ScoreValue/2);
 }
 
 void ADroneActor::Reached(float AddSpeedAmount)
 {
-	//TODO: Add animation for destruction.
 	if (this) HandleDeath();
 }
 
+/**
+ * @brief Handles death of drone.
+ */
 void ADroneActor::HandleDeath()
 {
 	Super::HandleDeath();
@@ -130,13 +140,14 @@ void ADroneActor::HandleDeath()
 	if (IsValid(this)) Destroy();
 }
 
+/**
+ * @brief Adjusts target location to avoid colliding with other drones.
+ */
 void ADroneActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	DL_NORMAL("Overlapping")
 	if (OtherActor->IsA<ADroneActor>())
 	{
-		DL_NORMAL("Adjusting target")
 		TargetLocation += AvoidanceRadius*(GetActorLocation() - OtherActor->GetActorLocation()).GetSafeNormal();
 	}
 }
