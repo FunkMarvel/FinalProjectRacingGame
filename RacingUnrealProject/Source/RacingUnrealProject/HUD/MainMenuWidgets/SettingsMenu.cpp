6bce296165146c2b35779865d461b2cad3a68274
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-05-20 03:56:00 +0200

    Update SettingsMenu.cpp

--- RacingUnrealProject/Source/RacingUnrealProject/HUD/MainMenuWidgets/SettingsMenu.cpp
+++ RacingUnrealProject/Source/RacingUnrealProject/HUD/MainMenuWidgets/SettingsMenu.cpp
@@ -5,6 +5,10 @@
 
 #include "Components/TextBlock.h"
 
+/**
+ * @brief Updates the displayed number of laps
+ * @param NumberToDisplay selected number of laps.
+ */
 void USettingsMenu::UpdateText(int32 NumberToDisplay)
 {
 	FString NumString{FString::Printf(TEXT("Number of laps: %01d"), NumberToDisplay)};
